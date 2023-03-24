50d9fbd35075629b0265b3a1459d200ac50ac688
atari-monk <atari.monk1@gmail.com>
2023-03-24 18:00:18 +0100

    First commit, project files and .gitignore

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
+    [SerializeField] private LayerMask countersLayerMask;
+
+    private bool isWalking;
+    private Vector3 lastInteractDir;
+
+    private void Start() {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e) {
+        var inputVector = gameInput.GetMovementVectorNormalized();
+        var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        if (moveDir != Vector3.zero) lastInteractDir = moveDir;
+        var interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void Update() {
+        HandleMovement();
+        HandleInteractions();
+    }
+
+    public bool IsWalking() {
+        return isWalking; 
+    }
+
+    private void HandleInteractions() {
+        var inputVector = gameInput.GetMovementVectorNormalized();
+        var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        if (moveDir != Vector3.zero) lastInteractDir = moveDir;
+        var interactDistance = 2f;
+        if (Physics.Raycast(transform.position, lastInteractDir, out RaycastHit raycastHit, interactDistance, countersLayerMask)) {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
+                //clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement() {
+        var inputVector = gameInput.GetMovementVectorNormalized();
+        var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        var playerRadius = .7f;
+        var playerHeight = 2f;
+        var canMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance) == false;
+
+        if (canMove == false) {
+            //Attempt only X movement
+            var moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance) == false;
+            if (canMove) {
+                moveDir = moveDirX;
+            } else {
+                //Attempt only Z movement
+                var moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance) == false;
+                if (canMove) {
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove) {
+            transform.position += moveDistance * moveDir;
+        }
+
+        isWalking = moveDir != Vector3.zero;
+
+        var rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+}
